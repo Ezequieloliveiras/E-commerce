b28a3e087fd34813acdb9a300c39e5ba9a4706d6
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-09-01 16:22:23 -0300

    adicionando estilos aos cards

--- src/componentes/Cards.js
+++ src/componentes/Cards.js
@@ -6,11 +6,9 @@ import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import FavoriteIcon from '@mui/icons-material/Favorite'
+import Button from '@mui/material/Button';
+
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-
-
 
 
 const Cards = ({
@@ -28,33 +26,51 @@ const Cards = ({
 
 
     return (
-        <Card sx={{ maxWidth: 345, height: '100%' }} style={{ padding: '20px' }}>
-            <CardMedia src={image}
-                component="img"
-                alt="Imagem do Produto"
-                style={{ width: '40%', display: 'block', margin: '0 auto' }}
-            />
+        <Card sx={{
+            maxWidth: 345,
+            height: '100%',
+            padding: '20px',
+            position: 'relative'
+        }}>
             <CardHeader
                 title={title}
                 src={image}
             />
+            <CardMedia style={{
+                width: '40%',
+                display: 'block',
+                margin: '0 auto'
+            }}
+                src={image}
+                component="img"
+                alt="Imagem do Produto"
+            />
             <CardContent>
-                <Typography variant="body2" color="text.secondary" >
-                    {showDescription ? description : `${description.substring(0, 100)}...`}
+                <Typography variant="body2" color="text.secondary" style={{marginBottom: '50px'}}>
+                    {
+                        showDescription ?
+                            description :
+                            `${description.substring(0, 100)}...`
+                    }
                     <IconButton onClick={toggleDescription} size='small'>
                         <ExpandMoreIcon />
                     </IconButton>
                 </Typography>
             </CardContent>
-            <CardActions disableSpacing style={{alignItems: 'end', justifyContent: 'center'}}>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
-                <div style={{ marginLeft: 'auto' }}>
-                    <IconButton aria-label="share">
-                        {`$ ${price}`}
-                    </IconButton>
-                </div>
+
+            <CardActions
+                style={{
+                    position: 'absolute',
+                    width: '85%',
+                    margin: '15px 0',
+                    bottom: 0,
+                    justifyContent: 'space-between',
+                }}>
+                <Button variant="contained">BUY</Button>
+
+                <p style={{ fontSize: 30 }}>
+                    {`$ ${price}`}
+                </p>
             </CardActions>
         </Card>
     )
--- src/templates/Padrão.js
+++ src/templates/Padrão.js
@@ -1,64 +1,54 @@
-
 import Container from '@mui/material/Container';
-import Header from "../partials/Header"
-import { styled } from '@mui/material/styles'
-import axios from 'axios';
-import { useState, useEffect } from 'react'
-import Cards from '../componentes/Cards';
+import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-
+import Header from "../partials/Header";
+import Cards from '../componentes/Cards';
 
 const StyledContainer = styled(Container)(({ theme }) => ({
-    paddingTop: 15,
-    
-}))
+  paddingTop: 15,
+}));
 
-
+const FixedSizeCards = ({ children }) => (
+  <Grid container justifyContent="center" alignItems="center">
+    {children}
+  </Grid>
+);
 
 const Padrão = ({ children }) => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([])
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+      .then(response => {
+        const data = response.data;
+        setProducts(data);
+      });
+  }, []);
 
-    console.log(products)
+  return (
+    <div style={{ background: '#e0e0e0' }}>
+      <Header />
+      <StyledContainer style={{ paddingLeft: "0px", paddingRight: "0px" }}>
+        {children}
+        <FixedSizeCards>
+          {products.map(item => (
+            <Grid item xs={12} md={4} style={{ width: '400px', height: '650px', padding: '15px', marginBottom:'60px' }} key={item.id}>
+              <Cards
+                title={item.title}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+              />
+            </Grid>
+          ))}
+        </FixedSizeCards>
+      </StyledContainer>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        axios.get('https://fakestoreapi.com/products')
-            .then(response => {
-                const data = response.data
-
-                setProducts(data)
-            })
-    }, [])
-
-    return (
-        <div style={{background: 'grey' }}>
-            <Header />
-            <StyledContainer style={{ paddingLeft: "0px", paddingRight: "0px" }}>
-                {children}
-                <Grid container>
-
-                    {
-                        products.map(item => (
-                            <Grid item xs={12} md={4} style={{padding: '30px'}}>
-                                <Cards
-                                    title={item.title}
-                                    price={item.price}
-                                    description={item.description}
-                                    category={item.category}
-                                    image={item.image}
-                                />
-                            </Grid>
-                        ))
-                    }
-
-                </Grid>
-
-            </StyledContainer>
-        </div>
-    )
-}
-
-
-
-export default Padrão
+export default Padrão;
