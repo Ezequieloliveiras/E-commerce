726e72c1e7c15284ea99a725c86deef5292dfdad
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-09-15 18:45:37 -0300

    montando o layout do carrinho

--- src/cards/CustomersCar.js
+++ src/cards/CustomersCar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Card from '@mui/material/Card'
+
 import Button from '@mui/material/Button'
 import DeleteIcon from '@mui/icons-material/Delete'
 import ModalConfirm from '../modal/ModalConfirm'
@@ -29,57 +29,86 @@ const Cards = ({
     }
 
     return (
-        <>
-            <CarText>
-                <h1 style={{
+        <CarText >
+            <h1 style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '30px'
+            }}>Product List</h1>
+
+            <div style={{ padding: '40px', display: 'flex', }}>
+
+                <div style={{
+                    width: '1300px',
+                    height: '800px',
+                    padding: '40px',
                     display: 'flex',
-                    justifyContent: 'center',
-                    marginBottom: '25px'
-                }}>Product List</h1>
-
-                <Card sx={{
-                    maxWidth: 500,
-                    height: '520px',
-                    marginLeft: '19px',
-                    marginTop: '90px',
-                    padding: '20px',
                     position: 'relative',
-                    display: 'block',
-                    alignItems: 'center',
-                    position: 'relative',
+                    boxShadow: '0px 0px 1px black',
+                    borderRadius: '5px',
+                    background: 'white',
                 }}>
-
-                    <p><h3>{title}</h3></p>
-                    <img src={image} alt={`Product ${id}`} style={{ maxWidth: '40%' }} />
-                    <p><h4>Price:</h4> {price}</p>
-                    <p><h4>Description:</h4> {description}</p>
-
-
                     <div style={{
+                        width: '900px',
+                        height: '500px',
                         display: 'flex',
                         justifyContent: 'center',
-                        alignItems: 'center',
+                        alignItems: 'center'
                     }}>
-                        <Button variant="outlined" onClick={handleRemoveCustomer} startIcon={<DeleteIcon />} style={{
-                            margin: '20px',
-                            position: 'absolute',
-                            bottom: '10px',
-                            color: 'red',
-                            border: '1px solid'
-                        }}>
-                            Delete
-                        </Button>
+                        <div style={{ marginBottom: '80px'}}>
+                            <img src={image} alt={`Product ${id}`} style={{ width: '300px' }} />
+                        </div>
                     </div>
-                </Card>
-                <ModalConfirm
-                    open={openModal}
-                    onClose={handleToggleOpenModal}
-                    onConfirm={handleConfirmModal}
-                    title='Deseja realmente excluir esse cadastro?'
-                    message='Ao confirmar nao será possível reverter essa operação'
-                />
-            </CarText>
-        </>
+                    <div style={{display:'flex'}}>
+                        <div style={{ width: '500px', height: '200px', padding:'25px' }}>
+                        <p><h2>{title}</h2></p>
+                            <p>segundo quadrado</p>
+                            <p><h4>Price:</h4> {price}</p>
+                        </div>
+                        <div>
+                            <div style={{
+                                width:'280px',
+                                height: '100%',
+                                borderRadius: '5px',
+                                boxShadow: '0px 0px 1px black',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                flexDirection: 'column',
+                                alignItems: 'center'
+                            }}>
+                                <h1>text</h1>
+                                <div style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}>
+                                    <Button variant="outlined" onClick={handleRemoveCustomer} startIcon={<DeleteIcon />} style={{
+                                        margin: '20px',
+                                        bottom: '10px',
+                                        color: 'red',
+                                        border: '1px solid'
+                                    }}>
+                                        Delete
+                                    </Button>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+                    <div style={{ bottom: '0', marginBottom: '80px', position: 'absolute', width:'700px' }}>
+                        <p><h4>Description:</h4> {description}</p>
+                    </div>
+                </div>
+            </div>
+            <ModalConfirm
+                open={openModal}
+                onClose={handleToggleOpenModal}
+                onConfirm={handleConfirmModal}
+                title='Deseja realmente excluir esse cadastro?'
+                message='Ao confirmar nao será possível reverter essa operação'
+            />
+        </CarText>
     )
 }
 
--- src/templates/CarText.js
+++ src/templates/CarText.js
@@ -1,8 +1,8 @@
 import { styled } from '@mui/material'
 import Typography from '@mui/material/Typography'
 
-const Container = styled('h1')({
-  padding: '30px 0 0 19px',
+const Container = styled('div')({
+  
 })
 
 const Title = ({children}) => {
